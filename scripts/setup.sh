6eac43b059155db62a06a0b1cbf39b6b0408224e
huohuo <62490976+G-huohuo@users.noreply.github.com>
2025-10-10 21:58:19 +0800

    Update setup.sh
    语言改成中文、时区改成上海

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,11 +3,18 @@
 DEBIAN_FRONTEND=noninteractive
 DEBCONF_NONINTERACTIVE_SEEN=true
 
-echo 'tzdata tzdata/Areas select Etc' | debconf-set-selections
-echo 'tzdata tzdata/Zones/Etc select UTC' | debconf-set-selections
-echo "locales locales/default_environment_locale select en_US.UTF-8" | debconf-set-selections
-echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8" | debconf-set-selections
-rm -f "/etc/locale.gen"
+# 设置时区为上海
+echo 'tzdata tzdata/Areas select Asia' | debconf-set-selections
+echo 'tzdata tzdata/Zones/Asia select Shanghai' | debconf-set-selections
+dpkg-reconfigure -f noninteractive tzdata
+
+# 设置系统语言为中文 UTF-8
+echo "locales locales/default_environment_locale select zh_CN.UTF-8" | debconf-set-selections
+echo "locales locales/locales_to_be_generated multiselect zh_CN.UTF-8 UTF-8" | debconf-set-selections
+dpkg-reconfigure -f noninteractive locales
+
+# 确保系统语言环境生效
+update-locale LANG=zh_CN.UTF-8
 
 apt update -qqy
 apt upgrade -qqy
