ed0808f0efd11ee49419538a1288463faf7edc2c
huohuo <62490976+G-huohuo@users.noreply.github.com>
2025-10-11 11:20:31 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,16 @@
 DEBIAN_FRONTEND=noninteractive
 DEBCONF_NONINTERACTIVE_SEEN=true
 
+# --- 修改为中国时区 (Asia/Shanghai) ---
+echo 'tzdata tzdata/Areas select Asia' | debconf-set-selections
+echo 'tzdata tzdata/Zones/Asia select Shanghai' | debconf-set-selections
+
+# --- 修改为中文语言环境 (zh_CN.UTF-8) ---
+# 将 zh_CN.UTF-8 设置为默认，同时保留 en_US.UTF-8 以获得更好的兼容性
+echo "locales locales/default_environment_locale select zh_CN.UTF-8" | debconf-set-selections
+echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8, zh_CN.UTF-8 UTF-8" | debconf-set-selections
+rm -f "/etc/locale.gen"
+
 apt update -qqy
 apt upgrade -qqy
 apt autoremove -qqy
@@ -31,19 +41,6 @@ apt install -qqy --no-install-recommends \
     wpasupplicant
 apt clean
 rm -rf /var/lib/apt/lists/*
-
-# 设置时区为上海
-echo 'tzdata tzdata/Areas select Asia' | debconf-set-selections
-echo 'tzdata tzdata/Zones/Asia select Shanghai' | debconf-set-selections
-dpkg-reconfigure -f noninteractive tzdata
-
-# 设置系统语言为中文 UTF-8
-echo "locales locales/default_environment_locale select zh_CN.UTF-8" | debconf-set-selections
-echo "locales locales/locales_to_be_generated multiselect zh_CN.UTF-8 UTF-8" | debconf-set-selections
-dpkg-reconfigure -f noninteractive locales
-
-# 确保系统语言环境生效
-update-locale LANG=zh_CN.UTF-8
 
 passwd -d root
 
